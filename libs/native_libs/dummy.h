c024526681338d0b80f4fdb5c0a69d7d94ff03d9
Piasy <xz4215@gmail.com>
2019-09-29 14:30:29 +0800

    win c interop works!

--- libs/native_libs/dummy.h
+++ libs/native_libs/dummy.h
@@ -5,6 +5,16 @@
 #ifndef KMPPBOOTSTRAP_DUMMY_H
 #define KMPPBOOTSTRAP_DUMMY_H
 
-const char* dummy_get_str();
+#ifdef WIN_VERSION
+#if defined(SELF_DUMMY_DLL)
+#define DUMMY_API __declspec(dllexport)
+#else
+#define DUMMY_API __declspec(dllimport)
+#endif
+#else
+#define DUMMY_API
+#endif
+
+DUMMY_API const char* dummy_get_str();
 
 #endif //KMPPBOOTSTRAP_DUMMY_H
